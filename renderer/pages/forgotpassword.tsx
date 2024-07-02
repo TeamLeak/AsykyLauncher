992d1b8400af39e5382157f1a75eef9452026f34
yataknemogy <efim@yataknemogy.com>
2024-07-02 05:37:57 +0300

    Анимация для страниц
    Реализация анимации при переходе между страниц с использованием библиотеки react-transition-group

--- renderer/pages/forgotpassword.tsx
+++ renderer/pages/forgotpassword.tsx
@@ -16,11 +16,12 @@ import Stack from '@mui/joy/Stack';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import BadgeIcon from '@mui/icons-material/Badge';
-
+import { CSSTransition } from 'react-transition-group';
 
 interface FormElements extends HTMLFormControlsCollection {
   email: HTMLInputElement;
 }
+
 interface SignInFormElement extends HTMLFormElement {
   readonly elements: FormElements;
 }
@@ -44,169 +45,169 @@ function ColorSchemeToggle(props: IconButtonProps) {
       }}
       {...other}
     >
-      {mode === 'light' ? <DarkModeIcon  /> : <LightModeIcon  />}
+      {mode === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
     </IconButton>
   );
 }
 
-export default function JoySignInSideTemplate() {
+export default function ForgotPassword() {
   return (
-    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
-      <CssBaseline />
-      <GlobalStyles
-        styles={{
-          ':root': {
-            '--Form-maxWidth': '800px',
-            '--Transition-duration': '0.4s',
-          },
-        }}
-      />
-      <Box
-        sx={(theme) => ({
-          width: { xs: '100%', md: '50vw' },
-          transition: 'width var(--Transition-duration)',
-          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
-          position: 'relative',
-          zIndex: 1,
-          display: 'flex',
-          justifyContent: 'flex-end',
-          backdropFilter: 'blur(12px)',
-          backgroundColor: 'rgba(255 255 255 / 0.2)',
-          [theme.getColorSchemeSelector('dark')]: {
-            backgroundColor: 'rgba(19 19 24 / 0.4)',
-          },
-        })}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100dvh',
-            width: '100%',
-            px: 2,
+    <CSSTransition in={true} appear={true} timeout={300} classNames="fade">
+      <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+        <CssBaseline />
+        <GlobalStyles
+          styles={{
+            ':root': {
+              '--Form-maxWidth': '800px',
+              '--Transition-duration': '0.4s',
+            },
           }}
+        />
+        <Box
+          sx={(theme) => ({
+            width: { xs: '100%', md: '50vw' },
+            transition: 'width var(--Transition-duration)',
+            transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+            position: 'relative',
+            zIndex: 1,
+            display: 'flex',
+            justifyContent: 'flex-end',
+            backdropFilter: 'blur(12px)',
+            backgroundColor: 'rgba(255 255 255 / 0.2)',
+            [theme.getColorSchemeSelector('dark')]: {
+              backgroundColor: 'rgba(19 19 24 / 0.4)',
+            },
+          })}
         >
           <Box
-            component="header"
             sx={{
-              py: 3,
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
-              <IconButton variant="soft" color="primary" size="sm">
-                <BadgeIcon  />
-              </IconButton>
-              <Typography level="title-lg">TeamLeak</Typography>
-            </Box>
-            <ColorSchemeToggle />
-          </Box>
-          <Box
-            component="main"
-            sx={{
-              my: 'auto',
-              py: 2,
-              pb: 5,
               display: 'flex',
               flexDirection: 'column',
-              gap: 2,
-              width: 400,
-              maxWidth: '100%',
-              mx: 'auto',
-              borderRadius: 'sm',
-              '& form': {
+              minHeight: '100dvh',
+              width: '100%',
+              px: 2,
+            }}
+          >
+            <Box
+              component="header"
+              sx={{
+                py: 3,
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+                <IconButton variant="soft" color="primary" size="sm">
+                  <BadgeIcon />
+                </IconButton>
+                <Typography level="title-lg">TeamLeak</Typography>
+              </Box>
+              <ColorSchemeToggle />
+            </Box>
+            <Box
+              component="main"
+              sx={{
+                my: 'auto',
+                py: 2,
+                pb: 5,
                 display: 'flex',
                 flexDirection: 'column',
                 gap: 2,
-              },
-              [`& .MuiFormLabel-asterisk`]: {
-                visibility: 'hidden',
-              },
-            }}
-          >
-            <Stack gap={4} sx={{ mb: 2 }}>
-              <Stack gap={1}>
-                <Typography component="h1" level="h3">
-                  Forgot Password
-                </Typography>
-                <Typography level="body-sm">
-                  Remembered password?{' '}
-                  <Link href="/home" level="title-sm">
-                    Sign in!
-                  </Link>
-                </Typography>
-              </Stack>
-            </Stack>
-            <Divider
-              sx={(theme) => ({
-                [theme.getColorSchemeSelector('light')]: {
-                  color: { xs: '#FFF', md: 'text.tertiary' },
+                width: 400,
+                maxWidth: '100%',
+                mx: 'auto',
+                borderRadius: 'sm',
+                '& form': {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: 2,
                 },
-              })}
+                [`& .MuiFormLabel-asterisk`]: {
+                  visibility: 'hidden',
+                },
+              }}
             >
-            </Divider>
-            <Stack gap={4} sx={{ mt: 2 }}>
-              <form
-                onSubmit={(event: React.FormEvent<SignInFormElement>) => {
-                  event.preventDefault();
-                  const formElements = event.currentTarget.elements;
-                  const data = {
-                    email: formElements.email.value,
-                  };
-                  alert(JSON.stringify(data, null, 2));
-                }}
-              >
-                <FormControl required>
-                  <FormLabel>Email</FormLabel>
-                  <Input type="email" name="email" />
-                </FormControl>
-                <Stack gap={2} sx={{ mt: 2 }}>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                    }}
-                  >
-                  </Box>
-                  <Button type="submit" fullWidth>
-                    Restore
-                  </Button>
+              <Stack gap={4} sx={{ mb: 2 }}>
+                <Stack gap={1}>
+                  <Typography component="h1" level="h3">
+                    Forgot Password
+                  </Typography>
+                  <Typography level="body-sm">
+                    Remembered password?{' '}
+                    <Link href="/home" level="title-sm">
+                      Sign in!
+                    </Link>
+                  </Typography>
                 </Stack>
-              </form>
-            </Stack>
-          </Box>
-          <Box component="footer" sx={{ py: 3 }}>
-            <Typography level="body-xs" textAlign="center">
-              © TeamLeak {new Date().getFullYear()}
-            </Typography>
+              </Stack>
+              <Divider
+                sx={(theme) => ({
+                  [theme.getColorSchemeSelector('light')]: {
+                    color: { xs: '#FFF', md: 'text.tertiary' },
+                  },
+                })}
+              />
+              <Stack gap={4} sx={{ mt: 2 }}>
+                <form
+                  onSubmit={(event: React.FormEvent<SignInFormElement>) => {
+                    event.preventDefault();
+                    const formElements = event.currentTarget.elements;
+                    const data = {
+                      email: formElements.email.value,
+                    };
+                    alert(JSON.stringify(data, null, 2));
+                  }}
+                >
+                  <FormControl required>
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" name="email" />
+                  </FormControl>
+                  <Stack gap={2} sx={{ mt: 2 }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                      }}
+                    />
+                    <Button type="submit" fullWidth>
+                      Restore
+                    </Button>
+                  </Stack>
+                </form>
+              </Stack>
+            </Box>
+            <Box component="footer" sx={{ py: 3 }}>
+              <Typography level="body-xs" textAlign="center">
+                © TeamLeak {new Date().getFullYear()}
+              </Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
-      <Box
-        sx={(theme) => ({
-          height: '100%',
-          position: 'fixed',
-          right: 0,
-          top: 0,
-          bottom: 0,
-          left: { xs: 0, md: '50vw' },
-          transition:
-            'background-image var(--Transition-duration), left var(--Transition-duration) !important',
-          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
-          backgroundColor: 'background.level1',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundImage:
-            'url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)',
-          [theme.getColorSchemeSelector('dark')]: {
+        <Box
+          sx={(theme) => ({
+            height: '100%',
+            position: 'fixed',
+            right: 0,
+            top: 0,
+            bottom: 0,
+            left: { xs: 0, md: '50vw' },
+            transition:
+              'background-image var(--Transition-duration), left var(--Transition-duration) !important',
+            transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+            backgroundColor: 'background.level1',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
             backgroundImage:
-              'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)',
-          },
-        })}
-      />
-    </CssVarsProvider>
+              'url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)',
+            [theme.getColorSchemeSelector('dark')]: {
+              backgroundImage:
+                'url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)',
+            },
+          })}
+        />
+      </CssVarsProvider>
+    </CSSTransition>
   );
 }
