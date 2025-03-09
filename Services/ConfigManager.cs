8b612ad339d84b83522ae737b9dd678037269400
ClonerNotFound <ClonerNotFound@gmail.com>
2025-03-10 00:30:02 +0300

    add settingspage

--- Services/ConfigManager.cs
+++ Services/ConfigManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Text.Json;
 using MauiApp3.Services.ConfigStrategies;
 
 namespace MauiApp3.Services
@@ -8,16 +8,34 @@ namespace MauiApp3.Services
         private readonly string _filePath;
         private readonly IConfigStrategy _strategy;
         private Dictionary<string, object> _config;
-
         public ConfigManager(string filePath, IConfigStrategy strategy)
         {
             _filePath = filePath;
             _strategy = strategy;
             _config = _strategy.LoadConfig(_filePath);
         }
-
         public void Set<T>(string key, T value) => _config[key] = value!;
-        public T? Get<T>(string key) => _config.ContainsKey(key) ? (T)Convert.ChangeType(_config[key], typeof(T)) : default;
+        public T? Get<T>(string key)
+        {
+            if (_config.ContainsKey(key))
+            {
+                if (_config[key] is T value)
+                {
+                    return value;
+                }
+                try
+                {
+                    var json = JsonSerializer.Serialize(_config[key]);
+                    return JsonSerializer.Deserialize<T>(json);
+                }
+                catch
+                {
+                    return default;
+                }
+            }
+
+            return default;
+        }
         public void Remove(string key) => _config.Remove(key);
         public void Save() => _strategy.SaveConfig(_filePath, _config);
     }
