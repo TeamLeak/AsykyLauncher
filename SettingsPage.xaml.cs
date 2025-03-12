0d2d5a9ffc61bd2bb597ac61b68f27e40f357d67
ClonerNotFound <clonernotfound@gmail.com>
2025-03-12 04:57:14 +0300

    JVM - added automatic parameter distribution by lines. Added automatic adding/updating of -XmxNB parameter, linked with RamSlider. Slightly improved interface

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,64 +1,93 @@
 using System.Collections.ObjectModel;
 using MauiApp3.Services.Providers;
 
-namespace MauiApp3;
-
-public partial class SettingsPage : ContentPage
+namespace MauiApp3
 {
-    private ObservableCollection<string> _jvmParams;
-    public SettingsPage()
+    public partial class SettingsPage : ContentPage
     {
-        InitializeComponent();
-
-        _jvmParams = new ObservableCollection<string>();
-
-        LoadJvmParams();
-
-        JvmParamsListView.ItemsSource = _jvmParams;
-    }
-
-    private void LoadJvmParams()
-    {
-        var configManager = ConfigProvider.GetConfigManager();
-
-        var savedParams = configManager.Get<List<string>>("JvmParams");
-
-        if (savedParams != null)
+        private ObservableCollection<string> _jvmParams;
+        public SettingsPage()
         {
-            foreach (var param in savedParams)
+            InitializeComponent();
+
+            _jvmParams = new ObservableCollection<string>();
+
+            LoadJvmParams();
+
+            BindingContext = this;
+
+            RamSlider.ValueChanged += RamSlider_ValueChanged;
+        }
+
+        public ObservableCollection<string> JvmParams => _jvmParams;
+
+        private void RamSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            string xmxParam = $"-Xmx{(int)e.NewValue}M";
+            UpdateOrAddXmxParam(xmxParam);
+        }
+
+        private void UpdateOrAddXmxParam(string xmxParam)
+        {
+            var existingParam = _jvmParams.FirstOrDefault(p => p.StartsWith("-Xmx"));
+            if (existingParam != null)
             {
-                _jvmParams.Add(param);
+                _jvmParams[_jvmParams.IndexOf(existingParam)] = xmxParam;
+            }
+            else
+            {
+                _jvmParams.Add(xmxParam);
+            }
+
+            SaveJvmParams();
+        }
+
+        private void LoadJvmParams()
+        {
+            var configManager = ConfigProvider.GetConfigManager();
+
+            var savedParams = configManager.Get<List<string>>("JvmParams");
+
+            if (savedParams != null)
+            {
+                foreach (var param in savedParams)
+                {
+                    _jvmParams.Add(param);
+                }
+            }
+
+            var xmxParam = _jvmParams.FirstOrDefault(p => p.StartsWith("-Xmx"));
+            if (xmxParam != null && int.TryParse(xmxParam.Replace("-Xmx", "").Replace("M", ""), out int xmxValue))
+            {
+                RamSlider.Value = xmxValue;
             }
         }
-    }
-
-    private void SaveJvmParams()
-    {
-        var configManager = ConfigProvider.GetConfigManager();
-        configManager.Set("JvmParams", _jvmParams.ToList());
-        configManager.Save();
-    }
-
-    private void BtnAddJvmParam_Clicked(object sender, EventArgs e)
-    {
-        if (!string.IsNullOrWhiteSpace(NewJvmParamEntry.Text))
+        private void SaveJvmParams()
         {
-            _jvmParams.Add(NewJvmParamEntry.Text);
-            NewJvmParamEntry.Text = string.Empty;
-
-            SaveJvmParams();
+            var configManager = ConfigProvider.GetConfigManager();
+            configManager.Set("JvmParams", _jvmParams.ToList());
+            configManager.Save();
         }
-    }
-
-    private void BtnRemJvmParam_Clicked(object sender, EventArgs e)
-    {
-        var button = sender as Button;
-        var param = button?.BindingContext as string;
-        if (param != null)
+        private void BtnAddJvmParam_Clicked(object sender, EventArgs e)
         {
-            _jvmParams.Remove(param);
+            if (!string.IsNullOrWhiteSpace(NewJvmParamEntry.Text))
+            {
+                _jvmParams.Add(NewJvmParamEntry.Text);
+                NewJvmParamEntry.Text = string.Empty;
 
-            SaveJvmParams();
+                SaveJvmParams();
+            }
+        }
+        private void BtnRemJvmParam_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var param = button?.BindingContext as string;
+            if (param != null)
+            {
+                _jvmParams.Remove(param);
+
+                SaveJvmParams();
+            }
         }
     }
 }
